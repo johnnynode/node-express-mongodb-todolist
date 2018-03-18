650662f72dc228620c7b6f9b645a0cf104b4bbf3
johnnynode <johnnynode@gmail.com>
2018-03-18 17:08:00 +0800

    add item success

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,16 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var data = [ {item: 'get milk'}, {item: "walk dog"}, {item: 'kick some coding ass'} ];
+
 module.exports = function (app) {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos:data});
   });
 
-  app.post('/todo', (req, res)=> {
-
+  app.post('/todo', urlencodedParser, (req, res)=> {
+    data.push(req.body);
+    res.json(data);
   });
 
   app.delete('/todo', (req, res) => {
